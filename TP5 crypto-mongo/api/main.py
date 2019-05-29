682bfb0958dd76888219c4e55f6f1cac3ff854f2
parafcyt <cuentafacultadgv@gmail.com>
2019-05-29 10:36:21 -0300

    agente funcionando y api en 0.0.0.0

--- TP5 crypto-mongo/api/main.py
+++ TP5 crypto-mongo/api/main.py
@@ -16,35 +16,7 @@ def get_db_conexion(url):
 
 db = get_db_conexion('mongodb://mongo-crypto:27017/')
 
-
-def get_documents():
-
-    params = {}
-
-    name = request.args.get('name', '')
-
-    limit = int(request.args.get('limit', 0))
-
-    if name:
-
-        params.update({'name': name})
-    
-    cursor = db.tickers.find(
-        params,
-        {'_id':0, 'ticker_hash': 0}
-    ).limit(limit)
-
-    return list(cursor)
-
-
-def get_top20():
-
-    pass
-
-
-def remove_currency():
-
-    pass
+print('hols')
 
 
 # RUTAS
@@ -56,14 +28,8 @@ def index():
     return 'CryptoApp API'
 
 
-@app.route('/top20', methods=['GET'])
-def top20():
-
-    return jsonify(get_top20())
-
-
 #CONSTRUIR IMAGEN
-#docker build -t="cryptomongo.api" .
+#docker build -t="cryptomongo-api" .
 
 #MONTAR CONTENEDOR
 #docker run -it --link=mongo-crypto:mongo-crypto -p 5000:5000 cryptomongo-api
