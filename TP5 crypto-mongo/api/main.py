79cedd7fd31231cde2c2acc4a9cfddec9c83c6b7
parafcyt <cuentafacultadgv@gmail.com>
2019-05-29 11:38:12 -0300

    api funcionando

--- TP5 crypto-mongo/api/main.py
+++ TP5 crypto-mongo/api/main.py
@@ -19,17 +19,36 @@ db = get_db_conexion('mongodb://mongo-crypto:27017/')
 print('hols')
 
 
+
+
 # RUTAS
 
 @app.route('/')
 def index():
 
-    #return jsonify({'name': 'CryptoApp API'})
-    return 'CryptoApp API'
+    resultados=[]
+    
+    for uno in db.tickers.find().limit(20):
+        uno.pop('_id')
+        resultados.append(uno)
 
+    #return jsonify({'name': 'CryptoApp API'})
+
+
+
+    return jsonify(resultados)
+
+
+#if __name__ == '__name__':
+#    app.run(host='localhost', port='5000')
 
 #CONSTRUIR IMAGEN
 #docker build -t="cryptomongo-api" .
 
 #MONTAR CONTENEDOR
 #docker run -it --link=mongo-crypto:mongo-crypto -p 5000:5000 cryptomongo-api
+
+#PARA NO TENER QUE REINICIAR CONTENEDOR
+#export FLASK_APP=main.py
+#export FLASK_DEBUG=1
+#flask run
